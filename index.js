f6b67cc0029b722f61a2d927b90ce4a29754c03c
Mark Vayngrib <mark@tradle.io>
2017-05-03 15:23:19 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+
+const { EventEmitter } = require('events')
+const Promise = require('any-promise')
+const RESOLVED = Promise.resolve()
+
+module.exports = function createSemaphore () {
+  let open
+
+  const ee = new EventEmitter()
+  ee.on('stop', () => open = false)
+  ee.on('go', () => open = true)
+
+  function go () {
+    ee.emit('go')
+    return api
+  }
+
+  function stop () {
+    ee.emit('stop')
+    return api
+  }
+
+  function wait () {
+    if (open) return RESOLVED
+
+    return new Promise(resolve => {
+      ee.once('go', resolve)
+    })
+  }
+
+  const api = { stop, go, wait }
+  return api
+}
